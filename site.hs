0cd5f4c4326397c88f55f35ec3ee6b97e5224794
Cj-bc <cj.bc-sd@outlook.jp>
2020-08-03 01:09:19 +0900

    Fix: "Logo" in Header hold constant value

--- site.hs
+++ site.hs
@@ -53,7 +53,7 @@ main = hakyll $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let indexCtx =
                     listField "posts" postCtx (return posts) `mappend`
-                    constField "title" blogName              `mappend`
+                    constField "title" ""                    `mappend`
                     defaultContext
 
             getResourceBody
