e0c4823c875b699011374abe35d6ec3c62c505d9
Cj-bc <cj.bc-sd@outlook.jp>
2021-01-02 19:52:34 +0900

    Change: Add class name for tags to use with fomantic-UI
    This code is not so neat, I want to refactor it someday

--- MyBlog/Contexts.hs
+++ MyBlog/Contexts.hs
@@ -3,8 +3,10 @@ module MyBlog.Contexts where
 import              Hakyll
 import qualified    Data.Attoparsec.Text as P
 import qualified Data.Text as T
+import Text.Blaze.Html (toHtml, toValue, (!))
+import qualified Text.Blaze.Html5.Attributes as A
+import qualified Text.Blaze.Html5 as H
 import              Shelly
-
 
 dateFormat :: T.Text
 dateFormat = "%B %e, %Y"
@@ -12,7 +14,7 @@ dateFormat = "%B %e, %Y"
 postCtx :: Tags -> Context String
 postCtx tags =
     dateField "date" (T.unpack dateFormat)
-    <> tagsField "tags" tags
+    <> tagsFieldWithFomanticClassName "tags" tags
     <> updateDataField
     <> teaserField "teaser" "raw content"
     <> thumbnailField
@@ -38,6 +40,21 @@ parseDate = do
 thumbnailField :: Context String
 thumbnailField = constField "thumbnail" "hard hat"
 
+-- | Almost same as 'tagsField', but this will give each tags class name for Fomantic-UI
+--
+-- TODO: support some Fomantic UI's variations?
+--       I want to make this not only for Fomantic but also for other customizations
+tagsFieldWithFomanticClassName = tagsFieldWith getTags simpleRenderLink' mconcat -- I don't know what is good function that alter mconcat.
+    where
+        -- | This function is copied and have some modification:
+        --   source: https://hackage.haskell.org/package/hakyll-4.13.4.1/docs/src/Hakyll.Web.Tags.html
+        simpleRenderLink' :: String -> (Maybe FilePath) -> Maybe H.Html
+        simpleRenderLink' _   Nothing         = Nothing
+        simpleRenderLink' tag (Just filePath) = Just $
+            H.a ! A.title (H.stringValue ("All pages tagged '"++tag++"'."))
+                ! A.href (toValue $ toUrl filePath)
+                ! A.class_ "ui tag label"
+                $ toHtml tag
 
 feedCtx = dateField "date" (T.unpack dateFormat)
           <> bodyField "description"
