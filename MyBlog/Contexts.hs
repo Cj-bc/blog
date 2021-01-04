ebea31a15fdaa14b55f8a6db2afd3a8e010365d8
Cj-bc <cj.bc-sd@outlook.jp>
2021-01-04 13:40:35 +0900

    Refactor: Adjust space

--- MyBlog/Contexts.hs
+++ MyBlog/Contexts.hs
@@ -7,6 +7,7 @@ import Text.Blaze.Html (toHtml, toValue, (!))
 import qualified Text.Blaze.Html5.Attributes as A
 import qualified Text.Blaze.Html5 as H
 import              Shelly
+
 defaultContext' :: Context String
 defaultContext' = constField "highlightjsTheme" "night-owl"
                 <> defaultContext
